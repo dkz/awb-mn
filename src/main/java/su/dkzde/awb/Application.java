ac4c2e7c290daf6b744a1a69617ffa34f992513d
Dmitry Kozlov <dkz>
2022-04-02 18:34:28 +0000

    Initial commit

---
+++ src/main/java/su/dkzde/awb/Application.java
@@ -0,0 +1,10 @@
+package su.dkzde.awb;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
